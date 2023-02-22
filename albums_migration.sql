217b2167f140a6565fd10e8253237082f0741249
Horace Smith <horacesmith26@gmail.com>
2023-02-22 09:57:08 -0600

    TABLE MYSQL EXERCISE

---
+++ albums_migration.sql
@@ -0,0 +1,15 @@
+create database if not exists codeup_test_db;
+use codeup_test_db;
+
+drop table if exists albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT;
+    artist VARCHAR (50) DEFAULT 'none',
+    name VARCHAR (50) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales FLOAT UNSIGNED NOT NULL,
+    genre TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
