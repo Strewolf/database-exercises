630b9fe46b19ed3e0f995e7d8ef2679bb191f49c
Horace Smith <horacesmith26@gmail.com>
2023-03-06 14:02:53 -0600

    where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,13 @@
 show databases;
 
-select * from employees.employees where first_name in ('irena','vidya','maya')
+select * from employees.employees where (first_name ='irena' or first_name = 'vidya' or first_name = 'maya') and  gender ='M';
+
+select * from employees.employees where last_name like 'E%';
+
+select * from employees.employees where last_name like '%q%';
+
+select * from employees.employees where last_name like 'E%' or last_name like '%E';
+
+select * from employees.employees where last_name like 'E%' and last_name like '%E';
+
+select * from employees.employees where last_name like '%q%' and last_name not like '%qu%';
