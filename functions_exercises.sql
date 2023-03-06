71eeaf7eb1593ec5b3c955fad3b9cabe342a9792
Horace Smith <horacesmith26@gmail.com>
2023-03-06 15:57:35 -0600

    limit exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -12,5 +12,9 @@ select concat(first_name,' ',last_name) as fullname from employees.employees whe
 
 select * from employees.employees where last_name like '%q%' and last_name not like '%qu%';
 
-select  * from employees.employees where year(hire_date) between 1990 and 2000 and month(birth_date) = 12 and day(birth_date)= 25 order by birth_date asc, hire_date desc;
+select  concat(first_name,' ',last_name,' has worked in the company for ', year(now()) - year(hire_date),' years') as fullname
+from employees.employees where year(hire_date) between 1990 and 2000
+and month(birth_date) = 12
+and day(birth_date)= 25
+order by birth_date asc, hire_date desc;
 
