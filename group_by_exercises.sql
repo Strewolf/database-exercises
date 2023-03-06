1587af73397c8e1700d88f7e907608470a47eaca
Horace Smith <horacesmith26@gmail.com>
2023-03-06 16:42:48 -0600

    limit exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,6 +1,19 @@
 
 use employees;
 select distinct title from titles;
+
 select distinct first_name, last_name
 from employees.employees where last_name like '%E' and last_name like 'E%'
 order by last_name,first_name;
+
+select distinct last_name from employees.employees
+where last_name like '%q%' and last_name not like '%qu%'
+group by last_name
+order by count(*) desc;
+
+select gender, count(*) as count
+from employees
+where first_name in ('irena','vidya','maya')
+group by gender
+order by count;
+
