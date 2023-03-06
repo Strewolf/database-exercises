54f26a66ed17eb4f203058dc7aa4b9cb204d9224
Horace Smith <horacesmith26@gmail.com>
2023-03-06 15:34:51 -0600

    limit exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,16 @@
+show databases;
+
+select * from employees.employees where (first_name ='irena' or first_name = 'vidya' or first_name = 'maya') order by last_name asc , first_name asc;
+
+select * from employees.employees where last_name like 'E%' order by emp_no desc;
+
+select * from employees.employees where last_name like '%q%';
+
+select * from employees.employees where last_name like 'E%' or last_name like '%E';
+
+select concat(first_name,' ',last_name) as fullname from employees.employees where last_name like 'E%' and last_name like '%E';
+
+select * from employees.employees where last_name like '%q%' and last_name not like '%qu%';
+
+select  * from employees.employees where  month(birth_date) = 12 and day(birth_date)= 25;
+
