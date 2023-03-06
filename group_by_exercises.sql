c035f8a740c769bdb7765de374bdeabafae879c5
Horace Smith <horacesmith26@gmail.com>
2023-03-06 16:10:22 -0600

    limit exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,3 +1,6 @@
-use titles;
+
 use employees;
 select distinct title from titles;
+select distinct first_name, last_name
+from employees.employees where last_name like '%E' and last_name like 'E%'
+order by last_name,first_name;
