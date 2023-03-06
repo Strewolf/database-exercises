5d35203f8754a30e7b283984d7c7f80d5a8c4122
Horace Smith <horacesmith26@gmail.com>
2023-03-06 10:46:59 -0600

    basic statements EXERCISE

---
+++ delete_exercises.sql
@@ -0,0 +1,8 @@
+use codeup_test_db;
+
+# Albums released after 1991
+delete from albums where release_date >1991;
+# Albums with the genre 'disco'
+delete from albums where genre = 'disco';
+# Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+delete from albums where artist = 'michael jackson';select * from albums;
