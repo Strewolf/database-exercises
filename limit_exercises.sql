0fb3bb2280b0304d3d8e9ace9fe73bc4ae615ad6
Horace Smith <horacesmith26@gmail.com>
2023-03-06 14:56:54 -0600

    limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,4 +2,4 @@ use employees;
 
 SELECT DISTINCT last_name FROM employees.employees order by last_name  desc limit 10;
 
-select emp_no from salaries order by salary desc limit 5;
+select emp_no from salaries order by salary desc limit 45,5;
