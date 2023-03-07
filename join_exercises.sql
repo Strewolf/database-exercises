033042e6e0bf08ad5701bd9690cf46a3bc834f18
Horace Smith <horacesmith26@gmail.com>
2023-03-07 15:21:46 -0600

    limit exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -58,3 +58,52 @@ FROM departments
          JOIN employees ON dept_manager.emp_no = employees.emp_no
 where dept_manager.to_date = '9999-01-01';
 
+SELECT departments.dept_name, CONCAT(employees.first_name, ' ', employees.last_name) AS manager_name
+FROM departments
+         JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+         JOIN employees ON dept_manager.emp_no = employees.emp_no
+where dept_manager.to_date = '9999-01-01' and  gender = 'F';
+
+
+SELECT titles.title AS title, COUNT(*) AS total
+FROM titles
+         JOIN employees ON titles.emp_no = employees.emp_no
+         JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+         JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE  departments.dept_name = 'customer service' and dept_emp.to_date = '9999-01-01' and titles.to_date = '9999-01-01'
+GROUP BY titles.title;
+
+SELECT departments.dept_name, CONCAT(employees.first_name, ' ', employees.last_name) AS department_manager, salaries.salary as salary
+FROM departments
+         JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+         JOIN employees ON dept_manager.emp_no = employees.emp_no
+join salaries on employees.emp_no = salaries.emp_no
+where dept_manager.to_date = '9999-01-01' and salaries.to_date = '9999-01-01';
+
+SELECT departments.dept_name as department,
+CONCAT(employees.first_name, ' ', employees.last_name) AS Employee,
+CONCAT(dept_manager.first_name, ' ', dept_manager.last_name) AS manager
+from departments
+JOIN dept_manager  on departments.dept_no = dept_manager.dept_no
+JOIN dept_emp on departments.dept_no = dept_emp.dept_no
+JOIN employees on departments.dept_no = dept_emp.dept_no AND dept_manager.emp_no = employees.emp_no
+where dept_manager.to_date = '9999-01-01' and dept_emp.to_date = '9999-01-01';
+
+select * from employees;
+select * from departments;
+select * from dept_manager;
+select * from dept_emp;
+
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee, d.dept_name AS Department, CONCAT(m.first_name, ' ', m.last_name) AS Manager
+FROM employees AS e
+         JOIN dept_emp AS de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01'
+         JOIN departments AS d ON de.dept_no = d.dept_no
+         JOIN dept_manager AS dm ON d.dept_no = dm.dept_no AND dm.to_date = '9999-01-01'
+         JOIN employees AS m ON dm.emp_no = m.emp_no;
+
+SELECT departments.dept_name, CONCAT(employees.first_name, ' ', employees.last_name)  ,CONCAT(employees.first_name, ' ', employees.last_name) AS manager
+FROM departments
+         JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+         JOIN employees ON dept_manager.emp_no = employees.emp_no
+where dept_manager.to_date = '9999-01-01';
