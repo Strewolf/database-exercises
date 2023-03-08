888008f183ea15f5e4b05604d79c3c41034a2210
Horace Smith <horacesmith26@gmail.com>
2023-03-08 09:43:41 -0600

    limit exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,3 +6,23 @@ where hire_date  in (
     from employees
     where emp_no = 101010
     );
+
+select title
+from titles
+where emp_no in (
+    select emp_no
+    from employees
+    where first_name = 'Aamod');
+
+select * from dept_manager
+where gender = f;
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    where gender = 'f' and to_date = '9999-01-01'
+);
+
+select * from dept_manager;
